7be3fe8f4204666f1e78b452821014e2b8925b53
Michail Tatsi <up875166@myport.ac.uk>
2019-03-29 12:08:33 +0000

    Reminders

--- Proto1C/src/proto1c/manager_Reminder.java
+++ Proto1C/src/proto1c/manager_Reminder.java
@@ -1,22 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proto1c;
+import java.time.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
 
 /**
  *
- * @author Josh
+ * @author UP875166
  */
 public class manager_Reminder {
-    private String name;
-    private int time;
-    private String location;
-    private String description;
+    private String reminderName;
+    private int reminderDay;
+    private int reminderMonth;
+    private int reminderYear;
+    private int reminderHour;
+    private int reminderMinute;
+    private String reminderLocation;
+    private String reminderDescription;
 
-    
-public manager_Reminder(){
-    
+
+public manager_Reminder(String name, int day, int month, int year, int hour, int minute,String location, String description){
+    name = reminderName;
+    day =  reminderDay;
+    month = reminderMonth;
+    year = reminderYear;
+    hour = reminderHour;
+    minute =  reminderMinute;
+    location = reminderLocation;
+    description = reminderDescription;
 }
+public static String getCurrentTimeStamp() {
+    SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//dd/MM/yyyy
+    Date now = new Date();
+    String strDate = sdfDate.format(now);
+    return strDate;
+}
+
+public String getReminderName(String name){
+    return name;
+}
+
+public String getLocation(String location){
+    return location;
+}
+
+public String getDescription(String description){
+    return description;
+}
+
 }
