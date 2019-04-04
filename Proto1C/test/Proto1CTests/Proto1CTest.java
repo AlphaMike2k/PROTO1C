55a89e968bafb1d11dea62a71c441d738f8d8b0b
Josh-Danks <45854486+Josh-Danks@users.noreply.github.com>
2019-04-04 16:09:35 +0100

    Build change
    Added Junit capabilty

---
+++ Proto1C/test/Proto1CTests/Proto1CTest.java
@@ -0,0 +1,16 @@
+package Proto1CTests;
+
+import org.junit.Test;
+import org.junit.Assert.*;
+
+/**
+ *
+ * @author Josh
+ */
+public class Proto1CTest {
+    
+    @Test
+    public void testConnection() {
+        
+    }
+}
